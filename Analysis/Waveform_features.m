d3b387f4e5e59af9aa0b3f90b781136f00cab983
JuanPimientoCaicedo <148992347+JuanPimientoCaicedo@users.noreply.github.com>
2024-01-17 11:40:05 -0500

    In development

---
+++ Analysis/Waveform_features.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Waveform_amps(inputArg1,inputArg2)
+%WAVEFORM_AMPS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
